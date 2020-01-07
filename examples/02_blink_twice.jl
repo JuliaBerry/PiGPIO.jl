e2deee5d6072402a6a0737ba9f3dc749d3547e58
Kim Fung <fungtszto@gmail.com>
2020-01-08 03:51:33 +0800

    Update 02_blink_twice.jl

--- examples/02_blink_twice.jl
+++ examples/02_blink_twice.jl
@@ -1,23 +1,25 @@
 
 using PiGPIO
 
-red_pin1 = 18
+red_pin1 = 18 #change these numbers accordingly with your GPIO pins
 red_pin2 = 23
 
-pinMode(red_pin1, OUTPUT)
-pinMode(red_pin2, OUTPUT)
+p=Pi()
+
+set_mode(p, red_pin1, PiGPIO.OUTPUT)
+set_mode(p, red_pin2, PiGPIO.OUTPUT)
 
 try 
-	for i in 1:10
-		digitalWrite(red_pin1, HIGH)
-        digitalWrite(red_pin2, LOW)
-		sleep(0.5)
-        digitalWrite(red_pin1, LOW)
-		digitalWrite(red_pin2, HIGH)
-		sleep(0.5)
-	end
+    for i in 1:10
+        PiGPIO.write(p, red_pin1, PiGPIO.HIGH)
+        PiGPIO.write(p, red_pin2, PiGPIO.LOW)
+        sleep(0.5)
+        PiGPIO.write(p, red_pin1, PiGPIO.LOW)
+        PiGPIO.write(p, red_pin2, PiGPIO.HIGH)
+        sleep(0.5)
+    end
 finally
     println("Cleaning up!")
-	pinMode(red_pin1, INPUT)
-    pinMode(red_pin2, INPUT)
+    set_mode(p, red_pin1, PiGPIO.INPUT)
+    set_mode(p, red_pin2, PiGPIO.INPUT)
 end
