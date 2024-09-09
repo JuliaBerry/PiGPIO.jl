931bf1c8a5a93afbe50b2bdf72ca391ce6b8b275
Alexander Barth <barth.alexander@gmail.com>
2024-09-09 18:53:20 +0200

    document option to make the documentation without a Raspberry Pi

--- docs/make.jl
+++ docs/make.jl
@@ -10,6 +10,9 @@ makedocs(
     sitename = "PiGPIO",
     format = Documenter.HTML(),
     modules = [PiGPIO],
+    # examples need to be run on a Raspberry Pi
+    # doctest = false,
+    # draft = true,
     pages = [
         "index.md",
         "Tutorial" => "tutorial.md",
